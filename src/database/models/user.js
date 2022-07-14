fa57a8f8b5e6fc224eeb06c75258ad507e2fb2fa
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-14 23:41:27 +0000

    feat: criacao do model user

---
+++ src/database/models/user.js
@@ -0,0 +1,31 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+
+  return User;
+};
+
+module.exports = User;
