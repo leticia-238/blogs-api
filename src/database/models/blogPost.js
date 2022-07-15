cd7e6e9a6073d71b9a621208bd9fb7ace225a9e4
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-15 22:31:44 +0000

    feat: criacao do model blogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,25 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    title: { allowNull: false, type: DataTypes.STRING },
+    content: { allowNull: false, type: DataTypes.STRING },
+    userId: { 
+      allowNull: false,
+      type: DataTypes.INTEGER, 
+      foreignKey: true, 
+    },
+    published: { allowNull: false, type: DataTypes.DATE },
+    updated: { allowNull: false, type: DataTypes.DATE },
+  });
+  
+  BlogPost.associate = ({ User }) => {
+    BlogPost.belongsTo(User, { foreignKey: 'userId' });
+  };
+  
+  return BlogPost;
+};
