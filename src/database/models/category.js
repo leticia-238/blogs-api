1b1d54ba79a57e9c12da07159c8fe10c08bede39
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-15 22:30:29 +0000

    feat: criacao do model category

---
+++ src/database/models/category.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: { allowNull: false, type: DataTypes.STRING },
+  });
+  
+  return Category;
+};
