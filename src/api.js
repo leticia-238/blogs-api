45a581399ea165cd9c9dc9b26eb0ebcd05eb2e91
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-18 14:52:36 +0000

    feat: criacao da rota post /login

--- src/api.js
+++ src/api.js
@@ -1,13 +1,10 @@
 const express = require('express');
-
-// ...
+const loginRouter = require('./routes/login');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', loginRouter);
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
