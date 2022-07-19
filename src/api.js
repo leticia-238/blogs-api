09db41fffae3ba79d26326f19044535aff7d19ca
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-19 22:41:04 +0000

    feat: criacao da rota post /user

--- src/api.js
+++ src/api.js
@@ -2,12 +2,15 @@ require('express-async-errors');
 const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const authRouter = require('./routes/auth');
+const userRouter = require('./routes/user');
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/login', authRouter);
+
+app.use('/user', userRouter);
 
 app.use(errorHandler);
 
