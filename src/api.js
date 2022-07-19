e65b48e45a72bc9fa6c41d0ab04d768f770adf50
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-07-19 00:37:30 +0000

    build: adiciona dependencia express-async-errors

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
+require('express-async-errors');
 const express = require('express');
-const loginRouter = require('./routes/login');
+const errorHandler = require('./middlewares/errorHandler');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/login', loginRouter);
+app.use('/login', authRouter);
+
+app.use(errorHandler);
 
 module.exports = app;
